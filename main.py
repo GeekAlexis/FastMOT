d0c7825dd9be6959e14883432cd3e073e6b6937d
Yukai Yang (Alexis) <yyang01@ucsb.edu>
2020-02-18 03:00:22 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import cv2
 import argparse
 import time
 import os
 from pathlib import Path
+import threading
+import cv2
 import objectdetector
 import bboxtracker
-import threading
+
 
 def gst_pipeline(
     capture_size=(1920, 1080),
@@ -46,7 +47,7 @@ def draw(frame, detections, tracks, acquire, track_id):
         cv2.putText(frame, 'Tracking %d' % track_id, (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, 0, 2, cv2.LINE_AA)
         
 
-def capture_frames(cap, frame_queue, capture_rate=None):
+def capture_frames(cap, frame_queue, cond, exit_event, capture_delay=None):
     while not exit_event.is_set():
         ret, frame = cap.read()
         with cond:
@@ -56,11 +57,11 @@ def capture_frames(cap, frame_queue, capture_rate=None):
                 break
             frame_queue.append(frame)
             cond.notify()
-        if capture_rate is not None:
-            time.sleep(1 / capture_rate)
+        if capture_delay is not None:
+            time.sleep(capture_delay)
 
 
-if __name__ == '__main__':
+def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('-i', '--input', help='Path to optional input video file')
     parser.add_argument('-o', '--output', help='Path to optional output video file')
@@ -92,36 +93,48 @@ if __name__ == '__main__':
     capture_size = (1920, 1080)
     camera_frame_rate = 30
     proc_size = (1280, 720)
-    detector_frame_skip = 8
+    acq_detector_frame_skip = 4
+    trk_detector_frame_skip = 6
     # classes = set([1])
     classes = set([1, 2, 3, 22, 24]) # person, bicycle, car, elephant, zebra
 
-    cap = cv2.VideoCapture(gst_pipeline(capture_size=capture_size, display_size=proc_size, frame_rate=camera_frame_rate, flip_method=args['flip']), cv2.CAP_GSTREAMER) if args['input'] is None else cv2.VideoCapture(args['input'])
+    cap = cv2.VideoCapture(gst_pipeline(capture_size, proc_size, camera_frame_rate, args['flip']), cv2.CAP_GSTREAMER) if args['input'] is None else cv2.VideoCapture(args['input'])
     if cap.isOpened():
         cond = threading.Condition()
         exit_event = threading.Event()
         frame_queue = []
         elapsed_time = 0
         frame_count = 0
-        capture_rate = camera_frame_rate if args['input'] is None else 40
-        # account for display and output overhead
-        if args['gui']:
-            capture_rate = 1 / (1 / capture_rate + 0.025)
-        if args['output'] is not None:
-            capture_rate = 1 / (1 / capture_rate + 0.06)
 
-        capture_thread = threading.Thread(target=capture_frames, args=(cap, frame_queue, capture_rate))
+        vid_fps = cap.get(cv2.CAP_PROP_FPS)
+        capture_dt = 1 / vid_fps
+        # account for display and output overhead so that capture queue won't overflow
+        capture_delay = capture_dt if args['input'] is None else 1 / 40 # video has max performance at 40 FPS
+        if args['gui']:
+            capture_delay += 0.025
+        if args['output'] is not None:
+            capture_delay += 0.06
+        if args['input'] is None:
+            # camera only grabs the most recent frame
+            capture_dt = capture_delay
+
+        capture_thread = threading.Thread(target=capture_frames, args=(cap, frame_queue, cond, exit_event, capture_delay))
         if args['enable_detector']:
-            print('[INFO] Loading detector model...')
-            detector = objectdetector.ObjectDetector(proc_size, classes=classes)
-            tracker = bboxtracker.BBoxTracker(proc_size, estimate_camera_motion=True)
+            objectdetector.ObjectDetector.init_backend()
+            print('[INFO] Loading tracking detector model...')
+            acq_detector = objectdetector.ObjectDetector(proc_size, classes, objectdetector.DetectorType.ACQUISITION)
+            print('[INFO] Loading acquisition detector model...')
+            trk_detector = objectdetector.ObjectDetector(proc_size, classes, objectdetector.DetectorType.TRACKING)
+            tracker = bboxtracker.BBoxTracker(proc_size, capture_dt)
             acquire = True
+            detector = acq_detector
+            detector_frame_skip = acq_detector_frame_skip
             acquisition_start_frame = 0
             track_id = -1
         if args['output'] is not None:
             Path(args['output']).parent.mkdir(parents=True, exist_ok=True)
             fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-            writer = cv2.VideoWriter(args["output"], fourcc, capture_rate, proc_size, True)
+            writer = cv2.VideoWriter(args["output"], fourcc, vid_fps, proc_size, True)
         print('[INFO] Starting capturing thread...')
         capture_thread.start()
 
@@ -140,8 +153,8 @@ if __name__ == '__main__':
                     frame = frame_queue.pop(0)
 
                 if frame_count == 0:
-                    print('[INFO] Video dimension %dx%d' % (frame.shape[1], frame.shape[0]))
-                if args['input'] is not None:
+                    print('[INFO] Video specs: %dx%d @ %d FPS' % (frame.shape[1], frame.shape[0], vid_fps))
+                if frame.shape[:2][::-1] != proc_size:
                     frame = cv2.resize(frame, proc_size)
                     # frame = cv2.medianBlur(frame, 3)
                 
@@ -150,74 +163,58 @@ if __name__ == '__main__':
                     if frame_count == 0:
                         detections = detector.detect_sync(frame)
                         tracker.init(frame, detections)
+                        print('[INFO] Acquiring new targets...')
                     else:
                         if acquire:
                             if frame_count - acquisition_start_frame == 100:
                                 if len(tracker.tracks) > 0:
                                     track_id = tracker.get_nearest_track()
                                     acquire = False
+                                    detector = trk_detector
+                                    detector_frame_skip = trk_detector_frame_skip
                                     print('[INFO] Following: %s' % tracker.tracks[track_id])
                                 else:
                                     acquisition_start_frame = frame_count
                         elif track_id not in tracker.tracks:
                             acquire = True
+                            detector = acq_detector
+                            detector_frame_skip = acq_detector_frame_skip
                             acquisition_start_frame = frame_count
-                            print('[INFO] Acquiring new targets')
+                            print('[INFO] Acquiring new targets...')
 
                         if frame_count % detector_frame_skip == 0:
-                            detector.preprocess(frame, tracker.tracks, acquire=acquire, track_id=track_id)
+                            # tic = time.perf_counter()
+                            detector.preprocess(frame, tracker.tracks, track_id=track_id)
                             detector.infer_async()
-                        tracker.track(frame)
-                        if frame_count % detector_frame_skip == 0:
+                            tracker.track(frame)
                             detections = detector.postprocess()
-                            tracker.update(detections, detector.roi, detector.tile_overlap, acquire=acquire)
-                        # else:
-                        #     tracks, H_camera = tracker.track(frame)
-                            # frame_small = cv2.resize(frame, None, fx=0.5, fy=0.5)
-                            # trackers = []
-                            # for det in detections:
-                            #     tracker = cv2.TrackerKCF_create()
-                            #     tracker.init(frame_small, det.bbox.scale(0.5, 0.5).cv_rect())
-                            #     trackers.append(tracker)
-
-                            # frame_small = cv2.resize(frame, None, fx=0.5, fy=0.5)
-                            # for i, tracker in enumerate(trackers):
-                            #     success, bbox = tracker.update(frame_small)
-                            #     if success:
-                            #         (x, y, w, h) = [int(2 * v) for v in bbox]
-                            #         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                            #     else:
-                            #         print('[INFO] Tracker failed')
-                            #         del trackers[i]
+                            # print('[INFO] Detector Latency: %.3f' % (time.perf_counter() - tic))
+                            tracker.update(detections, detector.cur_tile, detector.tile_overlap, acquire=acquire)
+                        else:
+                            tracker.track(frame)
 
                     if args['gui'] or args['output'] is not None:
                         # draw tracks and detections
                         draw(frame, detections, tracker.tracks, acquire, track_id)
-                        tracker.draw_bkg_feature_match(frame)
+                        tracker.flow_tracker.draw_bkg_feature_match(frame)
                         if frame_count % detector_frame_skip == 0:
-                            detector.roi.draw(frame)
+                            detector.cur_tile.draw(frame)
 
                 toc = time.perf_counter()
-                fps = round(1 / (toc - tic))
+                # fps = round(1 / (toc - tic))
                 elapsed_time += toc - tic
 
-                # tic = time.perf_counter()
                 if args['gui']:
                     # cv2.putText(frame, '%d FPS' % fps, (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, 0, 2, cv2.LINE_AA)
                     cv2.imshow('Video', frame)
                     if cv2.waitKey(1) & 0xFF == 27:
                         exit_event.set()
                         break
-                else:
-                    print('[INFO] FPS: %d' % fps)
-                # toc = time.perf_counter()
-                # print('gui', toc - tic)
+                # else:
+                #     print('[INFO] FPS: %d' % fps)
 
-                # tic = time.perf_counter()
                 if writer is not None:
                     writer.write(frame)
-                # toc = time.perf_counter()
-                # print('writer', toc - tic)
                 frame_count += 1
         finally:
             # clean up resources
@@ -232,3 +229,7 @@ if __name__ == '__main__':
         print('[INFO] Average FPS: %d' % avg_fps)
     else:
         raise Exception("Unable to open video stream")
+
+
+if __name__ == '__main__':
+    main()
