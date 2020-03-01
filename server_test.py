e33da3f12224301f17a7764837541d0619a82dd7
Yukai Yang (Alexis) <yyang01@ucsb.edu>
2020-02-29 22:35:45 -0800

    Create server_test.py

---
+++ server_test.py
@@ -0,0 +1,51 @@
+import socket
+from pathlib import Path
+import time
+
+MSG_LENGTH = 16
+
+class Msg:
+    """
+    enumeration type and function for socket messages
+    """
+    TARGET_NOT_FOUND, TARGET_ACQUIRED, TARGET_LOST, START, STOP, TERMINATE = ((i).to_bytes(MSG_LENGTH, byteorder='big') for i in range(6))
+
+    @staticmethod
+    def convert_bytes_to_bbox(byte_arr):
+        length = MSG_LENGTH // 4
+        return [int.from_bytes(byte_arr[i:i + length], byteorder='big') for i in range(0, MSG_LENGTH, length)]
+
+# delete socket path if it exists
+address = Path('/tmp/guardian_socket')
+if address.exists():
+    address.unlink()
+
+sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+
+sock.bind(str(address))
+sock.listen(1)
+print('server: Waiting for connection...')
+conn, client_addr = sock.accept()
+# conn.setblocking(False)
+print('server: connection from ', client_addr)
+
+conn.sendall(Msg.START)
+for i in range(50):
+    msg = conn.recv(MSG_LENGTH)
+    if msg:
+        #print('server', msg)
+        if msg == Msg.TARGET_NOT_FOUND:
+            print('server: target not found')
+        elif msg == Msg.TARGET_ACQUIRED:
+            print('server: target acquired')
+        elif msg == Msg.TARGET_LOST:
+            print('server: target lost')
+        else:
+            print('server:', Msg.convert_bytes_to_bbox(msg))
+
+conn.sendall(Msg.STOP)
+time.sleep(5)
+conn.sendall(Msg.START)
+time.sleep(5)
+conn.sendall(Msg.TERMINATE)
+conn.close()
