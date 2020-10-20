8c563dbf4b675e80812e438b3997621dcc87bb5c
Asad <asadjavedgujar@gmail.com>
2020-10-20 16:44:22 +0800

    Update install_jetson.sh
    Bracket was missing on [install_jetson.sh](https://github.com/GeekAlexis/FastMOT/blob/master/scripts/install_jetson.sh#L10)

--- scripts/install_jetson.sh
+++ scripts/install_jetson.sh
@@ -7,7 +7,7 @@ set -e
 
 # add CUDA paths
 echo 'export PATH=/usr/local/cuda/bin${PATH:+:${PATH}}' >> ~/.bashrc 
-echo 'export LD_LIBRARY_PATH=/usr/local/cuda/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}' >> ~/.bashrc 
+echo 'export LD_LIBRARY_PATH=/usr/local/cuda/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}' >> ~/.bashrc 
 source ~/.bashrc
 
 # install pip, numpy, pycuda, tensorflow, cython-bbox
