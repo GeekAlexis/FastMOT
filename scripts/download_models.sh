5ebe203ae241564f1326569ad54d8e099cb497f9
Alexis Yang <alexisyang555@gmail.com>
2020-11-09 12:16:31 -0800

    Downgrade OSNet to onnx opset10

--- scripts/download_models.sh
+++ scripts/download_models.sh
@@ -7,8 +7,8 @@ set -e
 
 pip3 install gdown
 
-gdown https://drive.google.com/uc?id=19APITvaqv_Ts5j3zkbo6MWCTvwTmka_T -O $DIR/osnet_ain_x1_0_msmt17.onnx
-gdown https://drive.google.com/uc?id=1hC3dFrjPM_olJ5utDCUkkMUqXIMIybKq -O $DIR/osnet_x0_25_msmt17.onnx
+gdown https://drive.google.com/uc?id=1dJ0ST2lgNlf1OrN4_PkRrWIN5FnH2nrE -O $DIR/osnet_ain_x1_0_msmt17.onnx
+gdown https://drive.google.com/uc?id=1MLC2lKnQvAQgBKZP1EXB6UdmqujY9qVd -O $DIR/osnet_x0_25_msmt17.onnx
 gdown https://drive.google.com/uc?id=1-Cqk2P72P4feYLJGtJFPcCxN5JttzTfX -O $DIR/ssd_inception_v2_coco.pb
 gdown https://drive.google.com/uc?id=1IfSveiXaub-L6PO9mqne5pk2EByzb25z -O $DIR/ssd_mobilenet_v1_coco.pb
 gdown https://drive.google.com/uc?id=1ste0fQevAjF4UqD3JsCtu1rUAwCTmETN -O $DIR/ssd_mobilenet_v2_coco.pb
