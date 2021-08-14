47dbfcbbd1e74efd6b7465d99f97d8bee08a35d5
Alexis Yang <alexisyang555@gmail.com>
2021-08-14 16:56:37 -0700

    Add --quiet option and rename --gui to --show

--- app.py
+++ app.py
@@ -14,32 +14,44 @@ from fastmot.utils import ConfigDecoder, Profiler
 
 def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument('-i', '--input_uri', metavar="URI", required=True, help=
-                        'URI to input stream\n'
-                        '1) image sequence (e.g. %%06d.jpg)\n'
-                        '2) video file (e.g. file.mp4)\n'
-                        '3) MIPI CSI camera (e.g. csi://0)\n'
-                        '4) USB camera (e.g. /dev/video0)\n'
-                        '5) RTSP stream (e.g. rtsp://<user>:<password>@<ip>:<port>/<path>)\n'
-                        '6) HTTP stream (e.g. http://<user>:<password>@<ip>:<port>/<path>)\n')
-    parser.add_argument('-c', '--config', metavar="FILE",
-                        default=Path(__file__).parent / 'cfg' / 'mot.json',
-                        help='path to JSON configuration file')
-    parser.add_argument('-l', '--labels', metavar="FILE",
-                        help='path to label names (e.g. coco.names)')
-    parser.add_argument('-o', '--output_uri', metavar="URI",
-                        help='URI to output video file')
-    parser.add_argument('-t', '--txt', metavar="FILE",
-                        help='output MOT Challenge txt results (e.g. eval/results/MOT20-01.txt)')
-    parser.add_argument('-m', '--mot', action='store_true', help='run multiple object tracker')
-    parser.add_argument('-g', '--gui', action='store_true', help='enable display')
-    parser.add_argument('-v', '--verbose', action='store_true', help='verbose output for debugging')
+    optional = parser._action_groups.pop()
+    required = parser.add_argument_group('required arguments')
+    group = parser.add_mutually_exclusive_group()
+    required.add_argument('-i', '--input-uri', metavar="URI", required=True, help=
+                          'URI to input stream\n'
+                          '1) image sequence (e.g. %%06d.jpg)\n'
+                          '2) video file (e.g. file.mp4)\n'
+                          '3) MIPI CSI camera (e.g. csi://0)\n'
+                          '4) USB camera (e.g. /dev/video0)\n'
+                          '5) RTSP stream (e.g. rtsp://<user>:<password>@<ip>:<port>/<path>)\n'
+                          '6) HTTP stream (e.g. http://<user>:<password>@<ip>:<port>/<path>)\n')
+    optional.add_argument('-c', '--config', metavar="FILE",
+                          default=Path(__file__).parent / 'cfg' / 'mot.json',
+                          help='path to JSON configuration file')
+    optional.add_argument('-l', '--labels', metavar="FILE",
+                          help='path to label names (e.g. coco.names)')
+    optional.add_argument('-o', '--output-uri', metavar="URI",
+                          help='URI to output video file')
+    optional.add_argument('-t', '--txt', metavar="FILE",
+                          help='path to output MOT Challenge format results (e.g. MOT20-01.txt)')
+    optional.add_argument('-m', '--mot', action='store_true', help='run multiple object tracker')
+    optional.add_argument('-s', '--show', action='store_true', help='show visualizations')
+    group.add_argument('-q', '--quiet', action='store_true', help='reduce output verbosity')
+    group.add_argument('-v', '--verbose', action='store_true', help='increase output verbosity')
+    parser._action_groups.append(optional)
     args = parser.parse_args()
+    if args.txt is not None and not args.mot:
+        raise parser.error('argument -t/--txt: not allowed without argument -m/--mot')
 
     # set up logging
     logging.basicConfig(format='%(asctime)s [%(levelname)8s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
     logger = logging.getLogger(fastmot.__name__)
-    logger.setLevel(logging.DEBUG if args.verbose else logging.INFO)
+    if args.quiet:
+        logger.setLevel(logging.WARNING)
+    elif args.verbose:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
 
     # load config file
     with open(args.config) as cfg_file:
@@ -56,21 +68,20 @@ def main():
     mot = None
     txt = None
     if args.mot:
-        draw = args.gui or args.output_uri is not None
+        draw = args.show or args.output_uri is not None
         mot = fastmot.MOT(config.resize_to, **vars(config.mot_cfg), draw=draw)
         mot.reset(stream.cap_dt)
-        if args.txt is not None:
-            assert Path(args.txt).suffix == '.txt'
-            Path(args.txt).parent.mkdir(parents=True, exist_ok=True)
-            txt = open(args.txt, 'w')
-    if args.gui:
+    if args.txt is not None:
+        Path(args.txt).parent.mkdir(parents=True, exist_ok=True)
+        txt = open(args.txt, 'w')
+    if args.show:
         cv2.namedWindow('Video', cv2.WINDOW_AUTOSIZE)
 
     logger.info('Starting video capture...')
     stream.start_capture()
     try:
         with Profiler('app') as prof:
-            while not args.gui or cv2.getWindowProperty('Video', 0) >= 0:
+            while not args.show or cv2.getWindowProperty('Video', 0) >= 0:
                 frame = stream.read()
                 if frame is None:
                     break
@@ -85,7 +96,7 @@ def main():
                             txt.write(f'{mot.frame_count},{track.trk_id},{tl[0]:.6f},{tl[1]:.6f},'
                                       f'{w:.6f},{h:.6f},-1,-1,-1\n')
 
-                if args.gui:
+                if args.show:
                     cv2.imshow('Video', frame)
                     if cv2.waitKey(1) & 0xFF == 27:
                         break
